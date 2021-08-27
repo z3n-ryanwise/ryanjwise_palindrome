450795c7c3d5b192dc087e4b23f01380d34df8b7
Ryan Wise <ryan.wise@zendesk.com>
2021-08-27 15:09:09 +1000

    Add tests

--- test/ryanjwise_palindrome_test.rb
+++ test/ryanjwise_palindrome_test.rb
@@ -1,13 +1,37 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class RyanjwisePalindromeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::RyanjwisePalindrome::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_non_palindrome
+    refute 'apple'.palindrome?
+  end
+
+  def test_literal_palindrome
+    assert 'racecar'.palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert 'RaceCar'.palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  def test_letters_removes_only_punctuation_native
+    assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
+  end
+
+  def test_letters_removes_only_punctuation_native_flipped
+    assert_equal "Madam, I'm Adam.".letters, "MadamImAdam"
+  end
+  
+  def test_letters_removes_only_punctuation_equals
+    assert "MadamImAdam" ==  "Madam, I'm Adam.".letters
   end
 end
